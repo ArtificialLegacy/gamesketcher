edde7b23e17414e1648b36620d1546ca0258958f
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2019-07-01 18:06:56 -0400

    Create themes.js

---
+++ themes.js
@@ -0,0 +1,108 @@
+const THEME = {
+  Dark: "DarkTheme",
+  Light: "LightTheme",
+  Midnight: "MidnightTheme",
+  Classic: "ClassicTheme",
+};
+
+const THEMES = {
+  DarkTheme: {
+    Editors: {
+      Background: "#696969",
+      Text: "#b3b3b3",
+      Grid: "#96a9b0",
+    },
+    Button: {
+      Background: "#9e9e9e",
+      Text: "#000000",
+      Border: "#000000",
+      Tooltip: {
+        Background: "#000000",
+        Text: "#ffffff",
+      },
+    },
+    MainText: "#c7c7c7",
+    UI: {
+      Background: "#7a7777",
+      Text: "#b3b3b3",
+      Error: "#ff6969",
+    },
+    Border: "#b3b3b3",
+    Background: "#3d3d3d",
+  },
+  
+  LightTheme: {
+    Editors: {
+      Background: "#cccccc",
+      Text: "#000000",
+      Grid: "#7d7d7d",
+    },
+    Button: {
+      Background: "#b5b5b5",
+      Text: "#000000",
+      Border: "#000000",
+      Tooltip: {
+        Background: "#000000",
+        Text: "#ffffff",
+      },
+    },
+    MainText: "#000000",
+    UI: {
+      Background: "#cccccc",
+      Text: "#000000",
+      Error: "#ff6969",
+    },
+    Border: "#696969",
+    Background: "#b8b8b8",
+  },
+  
+  MidnightTheme: {
+    Editors: {
+      Background: "#444444",
+      Text: "#b3b3b3",
+      Grid: "#96a9b0",
+    },
+    Button: {
+      Background: "#5e5e5e",
+      Text: "#000000",
+      Border: "#000000",
+      Tooltip: {
+        Background: "#777777",
+        Text: "#ffffff",
+      },
+    },
+    MainText: "#c7c7c7",
+    UI: {
+      Background: "#555555",
+      Text: "#b3b3b3",
+      Error: "#ff6969",
+    },
+    Border: "#b3b3b3",
+    Background: "#000000",
+  },
+  
+  ClassicTheme: {
+    Editors: {
+      Background: "#828282",
+      Text: "#000000",
+      Grid: "#000000",
+    },
+    Button: {
+      Background: "#A0A0A0",
+      Text: "#000000",
+      Border: "#000000",
+      Tooltip: {
+        Background: "#000000",
+        Text: "#ffffff",
+      },
+    },
+    MainText: "#000000",
+    UI: {
+      Background: "#828282",
+      Text: "#000000",
+      Error: "#ED886D",
+    },
+    Border: "#000000",
+    Background: "#3C3C3C",
+  },
+};
